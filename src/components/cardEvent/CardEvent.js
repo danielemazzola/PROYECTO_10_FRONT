8ad7fe72da7885d4ca961f9c5e3f9c54459020f8
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-06-22 12:05:07 +0200

    FEAT Search component

--- src/components/cardEvent/CardEvent.js
+++ src/components/cardEvent/CardEvent.js
@@ -3,101 +3,134 @@ import './helpers'
 import { date } from '../../utils/date'
 import { user } from '../../services/fetchIsAuth'
 import { MoreInfo, handleRegister } from './helpers'
+import { SearchBar } from '../search/Search'
 
 export const CardEvent = async (events, token) => {
   const reversedEvents = [...events.events].reverse()
   if (!events) return null
+
   const existContain = document.querySelector('#contain-events')
   if (!existContain) {
     app.innerHTML = `
       <div id="contain-events">
-          <p id="messageEvents"></p>
-          <p id="events-lenght"></p>
-          <div id="card-events">
-          </div>
+        <p id="messageEvents"></p>
+        <div id="search-container"></div>
+        <p id="events-length"></p>
+        <div id="card-events">
         </div>
+      </div>
     `
   }
-  const section = document.querySelector('#card-events')
+
+  const searchContainer = document.querySelector('#search-container')
   const pMessage = document.querySelector('#messageEvents')
-  const pEventsLength = document.querySelector('#events-lenght')
+  const pEventsLength = document.querySelector('#events-length')
+
   pMessage.textContent = events.message
   pEventsLength.textContent = `Total events: ${reversedEvents.length}`
-  reversedEvents.forEach((event) => {
-    const containerEvents = document.createElement('div')
-    containerEvents.id = event._id
-    const containerEventsWrapper = document.createElement('div')
-    const containerEvent = document.createElement('div')
-    containerEvents.classList.add('containerEvents')
-    containerEventsWrapper.classList.add('containerEventsWrapper')
-    containerEvent.classList.add('containerEvent')
-    const bannerEvent = document.createElement('div')
-    const imgEvent = document.createElement('img')
 
-    imgEvent.src = event.image
-    imgEvent.alt = event.title
-    bannerEvent.classList.add('bannerEvent')
-    const descriptionEvent = document.createElement('div')
-    const title = document.createElement('h2')
-    const dateP = document.createElement('span')
-    title.textContent = `${event.title}`
-    dateP.textContent = `Location: ${event.location} | ${date(
-      event.date
-    )} hours`
-    const description = document.createElement('p')
-    description.textContent = event.description
-    descriptionEvent.classList.add('descriptionEvent')
+  const renderEvents = (eventsToRender) => {
+    const section = document.querySelector('#card-events')
+    section.innerHTML = ''
 
-    const register = document.createElement('form')
-    const name = document.createElement('input')
-    const lastName = document.createElement('input')
-    const email = document.createElement('input')
-    const btnRegisterEvent = document.createElement('input')
+    eventsToRender.forEach((event) => {
+      const containerEvents = document.createElement('div')
+      containerEvents.id = event._id
+      const containerEventsWrapper = document.createElement('div')
+      const containerEvent = document.createElement('div')
+      containerEvents.classList.add('containerEvents')
+      containerEventsWrapper.classList.add('containerEventsWrapper')
+      containerEvent.classList.add('containerEvent')
+      const bannerEvent = document.createElement('div')
+      const imgEvent = document.createElement('img')
 
-    section.appendChild(containerEvents)
-    containerEvents.appendChild(containerEventsWrapper)
-    containerEventsWrapper.appendChild(containerEvent)
-    containerEvent.append(bannerEvent)
+      imgEvent.src = event.image
+      imgEvent.alt = event.title
+      bannerEvent.classList.add('bannerEvent')
+      const descriptionEvent = document.createElement('div')
+      const title = document.createElement('h2')
+      const dateP = document.createElement('span')
+      title.textContent = `${event.title}`
+      dateP.textContent = `Location: ${event.location} | ${date(
+        event.date
+      )} hours`
+      const description = document.createElement('p')
+      description.textContent = event.description
+      descriptionEvent.classList.add('descriptionEvent')
 
-    bannerEvent.appendChild(imgEvent)
-    descriptionEvent.append(title, dateP, description)
-    if (Object.keys(user).length === 0) {
-      register.classList.add('form-register-event')
-      name.name = 'name'
-      name.placeholder = 'Name: George'
-      name.required = true
-      lastName.name = 'lastName'
-      lastName.required = true
-      lastName.placeholder = 'Last name: Alvarez'
-      email.type = 'email'
-      email.required = true
-      email.name = 'email'
-      email.placeholder = 'E-mail: george@email.com'
-      btnRegisterEvent.type = 'submit'
-      btnRegisterEvent.value = 'Register'
-      btnRegisterEvent.classList.add('btn-register-event')
-      descriptionEvent.append(register)
-      register.append(name, lastName, email, btnRegisterEvent)
-      register.addEventListener('submit', async (e) => {
-        e.preventDefault()
-        handleRegister(event, register)
-      })
+      containerEvents.appendChild(containerEventsWrapper)
+      containerEventsWrapper.appendChild(containerEvent)
+      containerEvent.append(bannerEvent)
+
+      bannerEvent.appendChild(imgEvent)
+      descriptionEvent.append(title, dateP, description)
+
+      if (Object.keys(user).length === 0) {
+        const register = document.createElement('form')
+        const name = document.createElement('input')
+        const lastName = document.createElement('input')
+        const email = document.createElement('input')
+        const btnRegisterEvent = document.createElement('input')
+
+        register.classList.add('form-register-event')
+        name.name = 'name'
+        name.placeholder = 'Name: George'
+        name.required = true
+        lastName.name = 'lastName'
+        lastName.required = true
+        lastName.placeholder = 'Last name: Alvarez'
+        email.type = 'email'
+        email.required = true
+        email.name = 'email'
+        email.placeholder = 'E-mail: george@email.com'
+        btnRegisterEvent.type = 'submit'
+        btnRegisterEvent.value = 'Register'
+        btnRegisterEvent.classList.add('btn-register-event')
+        descriptionEvent.append(register)
+        register.append(name, lastName, email, btnRegisterEvent)
+        register.addEventListener('submit', async (e) => {
+          e.preventDefault()
+          handleRegister(event, register)
+        })
+      } else {
+        // TEMPLATE USER AUTH
+        const divBtn = document.createElement('div')
+        const btnMoreInfo = document.createElement('button')
+        divBtn.classList.add('event-more-information')
+        btnMoreInfo.classList.add('btn-more-information')
+        btnMoreInfo.textContent = 'More Information'
+        descriptionEvent.append(divBtn)
+        divBtn.append(btnMoreInfo)
+        btnMoreInfo.addEventListener('click', () => {
+          document
+            .querySelector('#header')
+            .scrollIntoView({ behavior: 'smooth', block: 'start' })
+          MoreInfo(event)
+        })
+      }
+
+      containerEvent.append(descriptionEvent)
+      section.appendChild(containerEvents)
+    })
+  }
+
+  const renderSearchBar = (onSearch) => {
+    const searchBarContainer = SearchBar(onSearch)
+    searchContainer.appendChild(searchBarContainer)
+  }
+
+  renderSearchBar((searchTerm) => {
+    if (searchTerm.trim() === '') {
+      renderEvents(reversedEvents)
+      pEventsLength.textContent = `Total events: ${reversedEvents.length}`
     } else {
-      // TEMPLATE USER AUTH
-      const divBtn = document.createElement('div')
-      const btnMoreInfo = document.createElement('button')
-      divBtn.classList.add('event-more-information')
-      btnMoreInfo.classList.add('btn-more-information')
-      btnMoreInfo.textContent = 'More Information'
-      descriptionEvent.append(divBtn)
-      divBtn.append(btnMoreInfo)
-      btnMoreInfo.addEventListener('click', () => {
-        document
-          .querySelector('#header')
-          .scrollIntoView({ behavior: 'smooth', block: 'start' })
-        MoreInfo(event)
-      })
+      const filteredEvents = reversedEvents.filter((event) =>
+        event.location.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      pEventsLength.textContent = `Total events: ${filteredEvents.length}`
+      renderEvents(filteredEvents)
     }
-    containerEvent.append(descriptionEvent)
   })
+
+  renderEvents(reversedEvents)
 }
---
+++ src/components/search/Search.js
@@ -0,0 +1,22 @@
+import './search.css'
+export const SearchBar = (onSearch) => {
+  const searchBarContainer = document.createElement('div')
+  searchBarContainer.classList.add('search-bar-container')
+
+  const searchInput = document.createElement('input')
+  searchInput.type = 'text'
+  searchInput.placeholder = 'Search by location...'
+  searchInput.classList.add('search-input')
+  searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase().trim()
+    onSearch(searchTerm)
+  })
+
+  const searchIcon = document.createElement('i')
+  searchIcon.classList.add('fa', 'fa-search', 'search-icon')
+
+  searchBarContainer.appendChild(searchInput)
+  searchBarContainer.appendChild(searchIcon)
+
+  return searchBarContainer
+}
